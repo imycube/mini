af4f3b2d493d2e74533a85854f1c1314573b6dd5
mini <largeclub@163.com>
2022-07-25 09:20:17 +0800

    add

---
+++ src/test/java/com/app/mini/MiniApplicationTests.java
@@ -0,0 +1,14 @@
+package com.app.mini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("test");
+    }
+
+}
